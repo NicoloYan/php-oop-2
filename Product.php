511ffed1e65cc10ef0e2e4dfff044bcf53df99ca
NicoloYan <nico.yan@outlook.it>
2022-07-29 15:15:00 +0200

    Created generic class for Product

---
+++ Product.php
@@ -0,0 +1,22 @@
+<?php
+
+class Product {
+
+    public $name;
+
+    public $brand; 
+
+    public $price = 0;
+
+    public function __construct($_name, $_brand, $_price) {
+        $this->name = $_name;
+        $this->brand = $_brand;
+        $this->price = $_price;
+    }
+
+    public function getDetails() {
+        return "$this->name $this->brand - Prezzo: $this->price euro.";
+    }
+}
+
+?>
